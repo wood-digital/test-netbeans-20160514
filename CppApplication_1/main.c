1ae40df75e021240f29c634746d3ce91e0234b02
wood-digital <mark.wood.digital@gmail.com>
2016-05-14 22:19:27 +0100

    initial commit

---
+++ CppApplication_1/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: mark
+ *
+ * Created on 14 May 2016, 21:44
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
